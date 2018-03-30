de2b4e9813af9c78a7ba1bd04e5b64efbde2ec9f
uchihasks <sakets594@gmail.com>
2018-03-30 17:12:27 +0530

    Update CO92JUDG.cpp

--- codechef/march18_cookoff/CO92JUDG.cpp
+++ codechef/march18_cookoff/CO92JUDG.cpp
@@ -12,3 +12,46 @@ Note that x can be different from y; the index chosen by Alice does not affect B
 
 Chef, as the judge, needs to announce the result of the competition. He knows that both Alice and Bob play optimally and will always choose the best option. Please help Chef determine the result
 */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int t;
+	int n;
+	cin>>t;
+	long long a[105],b[105];
+	long long s1,s2;
+	while(t--)
+	{
+		s1=s2=0;
+		cin>>n;
+		for(int i=0;i<n;i++)
+			cin>>a[i];
+		for(int i=0;i<n;i++)
+			cin>>b[i];
+		sort(a,a+n);
+		sort(b,b+n);
+		for(int i=0;i<n-1;i++){
+			s1+=a[i];
+			s2+=b[i];
+		}
+		//cout<<s1<<" "<<s2<<" ";
+		if(s1==s2)
+		{
+			cout<<"Draw"<<endl;
+		}
+		else  if(s1<s2)
+		{
+			cout<<"Alice"<<endl;
+		}
+		else 
+		{
+			cout<<"Bob"<<endl;
+		}
+
+
+	}
+	return 0;
+}
